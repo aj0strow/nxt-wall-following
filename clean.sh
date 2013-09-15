582614ea4cc6e9b6863cc4056a8d8f7e876c4993
aj0strow <alexander.ostrow@gmail.com>
2013-09-14 22:19:20 -0400

    added output to scripts

--- clean.sh
+++ clean.sh
@@ -1,2 +1,4 @@
-rm classes/*.class;
-rm *.nxj;
+echo -n "cleaning project... ";
+rm classes/*.class > /dev/null 2>&1;
+rm *.nxj > /dev/null 2>&1;
+echo "complete!";
--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 source clean.sh;
+echo -n "compiling project... ";
 find src -type f -name *.java | xargs nxjc -d classes;
 cd classes && nxjlink -o ../Lab1.nxj Lab1;
-cd ..;
+cd .. && echo "complete!"
